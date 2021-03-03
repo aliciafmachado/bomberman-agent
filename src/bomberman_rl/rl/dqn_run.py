37698a8d9f80469e99b4b34971588ea5c985646d
Alicia Fortes Machado <aliciafortesmachado@gmail.com>
2021-03-03 23:36:28 +0100

    Adding dqn_run not implemented

---
+++ src/bomberman_rl/rl/dqn_run.py
@@ -0,0 +1,20 @@
+# Run main for DQNAgent
+
+from bomberman_rl.rl.ReplayMemory import ReplayMemory
+from bomberman_rl.rl.ReplayMemory import Simulation
+
+MEMORY_SIZE=10000
+
+# TODO
+# Return if the there are not experiences enough to train
+        # the agent
+        if(len(self.memory)) < batch_size:
+            return
+
+        sample = self.memory.sample(batch_size)
+
+                # We transpose the simulations in order to get 
+        # the actions and rewards in the standart way
+        batch = Simulation(*zip(*sample))
+
+        self.memory = ReplayMemory(MEMORY_SIZE)
