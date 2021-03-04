93172d56fd666fbf301019b02c941ccd75e55083
Aloysio <aloysiogl@gmail.com>
2021-03-04 16:12:33 +0100

    Add base abstract classes for agents

--- src/bomberman_rl/agent/agent.py
+++ src/bomberman_rl/agent/agent.py
@@ -1,9 +1,18 @@
+from bomberman_rl.envs.conventions import UP, DOWN, LEFT, RIGHT, STOP, PLACE_BOMB
+
+
 class Agent:
     """
-    Base class for an agent that takes avalible_actions on and environment
+    Base class for an agent that takes actions on and environment
     """
+
+    AVAILABLE_ACTIONS = [UP, DOWN, LEFT, RIGHT, STOP, PLACE_BOMB]
+
     def __init__(self):
         pass
 
     def choose_action(self, *args):
+        """
+        Chooses the next agent the agent should take given a state
+        """
         raise NotImplementedError
